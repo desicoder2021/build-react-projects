e6fa664ce926e235e0f94408c8eeba374c874978
desicoder2021 <yusufkasmani491@gmail.com>
2022-10-05 10:39:01 +0530

    starter file modified

--- P01-Pomodoro/start/src/App.js
+++ P01-Pomodoro/start/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h2>Pomodoro!</h2>
+
+      <div className='timer'>
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+      </div>
+
+      <div className='buttons'>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
