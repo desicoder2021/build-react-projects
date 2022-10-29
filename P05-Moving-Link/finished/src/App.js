9261f6ff923470b94603d7c8aa770199b578a1a4
desicoder2021 <yusufkasmani491@gmail.com>
2022-10-29 23:22:28 +0530

    files updated

--- P05-Moving-Link/finished/src/App.js
+++ P05-Moving-Link/finished/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const canvasRef = useRef(null);
+
+  // setting the height and width of canvas
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+  }, []);
+
+  // move the box if x or y changes
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.clearRect(0, 0, window.innerHeight, window.innerWidth);
+    context.fillRect(x, y, 100, 100);
+  }, [x, y]);
+
+  // add event listener to window to listen for arrow keys
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    function handleKeyDown(e) {
+      console.log(e.key, e);
+      if (e.key === "ArrowUp") move("up");
+      if (e.key === "ArrowLeft") move("left");
+      if (e.key === "ArrowDown") move("down");
+      if (e.key === "ArrowRight") move("right");
+    }
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  });
+
+  function move(direction) {
+    if (direction === "up") setY((y) => y - 20);
+    if (direction === "left") setX((x) => x - 20);
+    if (direction === "down") setY((y) => y + 20);
+    if (direction === "right") setX((x) => x + 20);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <canvas ref={canvasRef} />
+
+      <div className='arrows'>
+        <button onClick={() => move("up")}>Up</button>
+        <button onClick={() => move("left")}>Left</button>
+        <button onClick={() => move("down")}>Down</button>
+        <button onClick={() => move("right")}>Right</button>
+      </div>
     </div>
   );
 }
